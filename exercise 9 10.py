a21a16dfca8df37ee4133a11afddc0ae04d2ea87
ArnoldCody <arnoldyd@outlook.com>
2016-08-13 10:24:58 +0800

    ex 10

--- exercise 9 10.py
+++ exercise 9 10.py
@@ -8,10 +8,18 @@ time.sleep(...)
         Delay execution for a given number of seconds.  The argument may be
         a floating point number for subsecond precision.
 """
-
+# ex9
 import time
 
 myD = {1: 'a', 2: 'b'}
 for key, value in dict.items(myD):
 	print key, value
 	time.sleep(1) # 暂停 1 秒
+
+# ex10
+print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+
+# 暂停一秒
+time.sleep(1)
+
+print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
